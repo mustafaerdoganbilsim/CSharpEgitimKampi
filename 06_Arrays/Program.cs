cb4503afea64bb48a76e79eb38bc0c4272bf4e2d
Mustafa Erdoğan <merdogan038@hotmail.com>
2024-10-13 20:12:38 +0300

    .

--- 06_Arrays/Program.cs
+++ 06_Arrays/Program.cs
@@ -10,6 +10,77 @@ namespace _06_Arrays
     {
         static void Main(string[] args)
         {
+
+            #region Temel Dizi Örnekleri
+
+            //string[] color = new string[4];
+
+            //color[0] = "Kırmızı";
+            //color[1] = "Sarı";
+            //color[2] = "Beyaz";
+            //color[3] = "Mavi";
+
+            //Console.WriteLine(color[2]);
+
+
+            //string[] cities = new string[5];
+
+            //cities[0] = "Milano";
+            //cities[1] = "Budapeşte";
+            //cities[2] = "Lyon";
+            //cities[3] = "Kahire";
+            //cities[4] = "Üsküp";
+
+            //Console.WriteLine(cities[4]);
+
+            #endregion
+
+            #region Dizideki tüm elemanları listeleme
+
+            //string[] colors = { "Sarı", "Kırmızı", "Beyaz", "Mavi", "Yeşil", "Turuncu", "Pembe" };
+
+            //for (int i = 0; i < colors.Length; i++)
+
+            //{
+            //    Console.WriteLine(colors[i]);
+            //}
+
+            //int[] numbers = { 4, 85, 96, 75, 125, 635, 488, 522, 7456, 2365, 1120 };
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 3 == 0)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+
+
+            //char[] sembols = {'a','b','c','*','/','-' };
+
+            //for (int i = 0; i <sembols.Length; i++)
+            //{
+            //    Console.WriteLine(sembols[i]);
+            //}
+
+            // dizideki en büyük sayyı bulma
+
+            //int[] myArray = { 47, 85, 95, 41, 25, 635, 789, 86, 100 };
+
+            //int maxNumbers = myArray[0];
+
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+            //    if (myArray[i] > maxNumbers)
+            //    {
+            //        maxNumbers = myArray[i];
+
+            //    }
+            //}
+
+            //Console.WriteLine(maxNumbers);
+            #endregion
+            Console.Read();
         }
     }
 }
