647059a792bc8f12888ae4ff6f76854c8781b5da
mustafaerdoganbilsim <mustafaerdoganbilisim@gmail.com>
2024-10-13 18:55:25 +0300

    Add files via upload

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt alta 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <=10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region Yan yana 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <=10; i++)
+            //{
+            //   Console.Write("*");
+            //}
+
+            #endregion
+
+            #region Alt alta 10 tane yıldız oluşturma her satırda 10 tane yıldız olsun
+
+            //for (int i = 1; i <=10; i++)
+            //{
+            //   Console.WriteLine("**********");
+            //}
+
+            #endregion
+
+            #region Dik üçgen yıldız oluşturma
+
+            //for (int i = 1; i <=5; i++)
+            //{
+            //    for(int j = 1; j <=i; j++) 
+            //    Console.Write("*");
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters dik üçgen yıldız oluşturma
+
+
+            //for (int i = 5; i >=1; i--)
+            //{
+            //    for (int j = 1; j <=i; j++) 
+
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+
+            #region Ters ve Dik Üçgen Beraber
+
+            //for (int i = 1; i <= 5; i++)
+
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+
+            //}
+
+            //for (int k = 4; k >= 1; k--)
+
+            //{
+            //    for (int m = 1; m <= k; m++)
+            //    {
+
+            //        Console.Write('*');
+
+            //    }
+            //    Console.WriteLine();
+
+
+            //}
+
+            #endregion
+
+
+            Console.Read();
+
+        }
+    }
+}
