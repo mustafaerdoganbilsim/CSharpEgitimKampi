23a5a4e8f631bcb79c832a07b80577765787e64c
Mustafa Erdoğan <merdogan038@hotmail.com>
2024-10-19 15:53:19 +0300

    .

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach Döngüsü
+
+            //string[] cities = {"milano","roma","budapeşte","ankara" };
+
+            //foreach (string x in cities)
+
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
